9472e91ab7962c979e9152dce1a1f2a327de1335
Diego Segovia <ogeid786@gmail.com>
2026-05-23 19:16:23 -0400

    Add past dialysis logic

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const actionBtn = document.getElementById("actionBtn");
-  const dataInput = document.getElementById("dataInput");
+  const currentKLevelElement = document.getElementById("currentK+Level");
+  const firstKLevelElement = document.getElementById("1stK+Level");
+  const secondKLevelElement = document.getElementById("2ndK+Level");
+  const thirdKLevelElement = document.getElementById("3rdK+Level");
   const feedbackArea = document.getElementById("feedbackArea");
   const feedbackAreaPlaceholder = document.getElementById(
     "feedbackAreaPlaceholder",
   );
   const bathAmount = document.getElementById("bathAmountId");
+  const dialysisFrequencyResult = document.getElementById(
+    "dialysisFrequencyResult",
+  );
 
   actionBtn.addEventListener("click", () => {
-    const inputValue = dataInput.value;
+    const currentKLevel = currentKLevelElement.value;
+    const firstKLevel = firstKLevelElement.value;
+    const secondKLevel = secondKLevelElement.value;
+    const thirdKLevel = thirdKLevelElement.value;
 
-    if (!inputValue) {
+    if (!currentKLevel) {
       feedbackAreaPlaceholder.textContent = "Please enter a valid number.";
       feedbackAreaPlaceholder.classList.add("text-danger");
       feedbackAreaPlaceholder.classList.remove("text-body-secondary");
@@ -22,18 +31,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
     feedbackAreaPlaceholder.classList.add("d-none");
     feedbackArea.classList.remove("d-none");
+    dialysisFrequencyResult.classList.remove("d-none");
 
-    if (inputValue >= 6) {
+    if (firstKLevel == 2 && secondKLevel == 2 && thirdKLevel == 2) {
+      dialysisFrequencyResult.textContent = "Patient can come off weekly labs.";
+    } else {
+      dialysisFrequencyResult.textContent = "Patient will be on weekly labs.";
+    }
+
+    if (currentKLevel >= 6) {
       bathAmount.textContent = "1.0 K+";
       return;
     }
 
-    if (inputValue < 6 && inputValue > 4) {
+    if (currentKLevel < 6 && currentKLevel > 4) {
       bathAmount.textContent = "2.0 K+";
       return;
     }
 
-    if (inputValue <= 4) {
+    if (currentKLevel <= 4) {
       bathAmount.textContent = "3.0 K+";
       return;
     }
